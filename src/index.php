e31afdf869ae1ac9fe465fd9dd3d870161469b47
Safiye <safiye.akdede@hotmail.com>
2020-03-02 16:10:50 +0100

    crea de l'index du login et du register

--- src/index.php
+++ src/index.php
@@ -6,16 +6,40 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+        <link rel="stylesheet" href="./styles/style.css"/>
         <title>BCBB</title>
     </head>
     <body>
-
-    
-
+        <div class="main-container">
+            <ul class="nav justify-content-end">
+                <li class="nav-item bg-secondary">
+                    <a class="nav-link text-white" href="register.php">Sign up</a>
+                </li>
+                <li class="nav-item bg-secondary">
+                    <a class="nav-link text-white" href="login.php">Sign in</a>
+                </li>
+            </ul>
+            <h1>BCBB</h1>
+            <ul class="nav nav-justified">
+                <li class="nav-item bg-secondary">
+                    <a class="nav-link text-white" href="">General</a>
+                </li>
+                <li class="nav-item bg-secondary">
+                    <a class="nav-link text-white" href="">Development</a>
+                </li>
+                <li class="nav-item bg-secondary">
+                    <a class="nav-link text-white" href="">Smalltalk</a>
+                </li>
+                <li class="nav-item bg-secondary">
+                    <a class="nav-link text-white" href="">Events</a>
+                </li>
+            </ul>
+            <div class="board-topic-container">
+                <div class="board-topic-elements"></div>
+            </div>
+        </div>
         <!-- Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
