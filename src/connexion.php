1b3ebe72ab1bbced9e27b7c2a9fff260cb7daa6c
Arnaud Magonette <arnaud.magonette@gmail.com>
2020-03-04 11:57:15 +0100

    ajout modification et suppression des messages

---
+++ src/connexion.php
@@ -0,0 +1,18 @@
+<?php
+
+// CONNEXION A LA BD  -----------------------------------------------
+// Paramètres de connexion au serveur de données
+$host = 'mysql';
+$user = 'root';
+$pass = 'root';
+$dbname = 'mydb';
+
+$dsn = "mysql:host=$host;dbname=$dbname";
+
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+
+try {
+    $pdo = new \PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int) $e->getCode());
+}
