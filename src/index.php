7cf501f3cd9c7cb964a3696e068652ebbd032772
Safiye <safiye.akdede@hotmail.com>
2020-03-12 09:18:20 +0100

    petites modification

--- src/index.php
+++ src/index.php
@@ -36,7 +36,7 @@
                         } else {
                             echo '<a class="nav-link text-white bg-secondary" href="index.php?board=Development">Development</a>';
                         }
-                    ?>                
+                    ?>
                 </li>
                 <li class="nav-item">
                     <?php
@@ -45,7 +45,7 @@
                         } else {
                             echo '<a class="nav-link text-white bg-secondary" href="index.php?board=Smalltalk">Smalltalk</a>';
                         }
-                    ?>                
+                    ?>
                 </li>
                 <li class="nav-item">
                     <?php
@@ -54,7 +54,7 @@
                         } else {
                             echo '<a class="nav-link text-white bg-secondary" href="index.php?board=Events">Events</a>';
                         }
-                    ?>                
+                    ?>
                 </li>
             </ul>
             <?php
@@ -167,8 +167,7 @@
             ?>
         </div>
         <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="scrollBar.js">
-    </script>
+        <script src="scrollBar.js"></script>
         <!-- Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
