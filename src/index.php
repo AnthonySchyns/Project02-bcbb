95a4a5afb22969b98396e8a4e32555cc7856d248
Safiye <safiye.akdede@hotmail.com>
2020-03-12 10:54:19 +0100

    ajout de la description

--- src/index.php
+++ src/index.php
@@ -30,8 +30,16 @@ function getTopics(PDO $pdo, $a){
     $sth->closeCursor();
     $sth = null;
 }
-
-
+function getDescription(PDO $pdo, $i){
+    $sql = "SELECT description "
+        . "FROM boards "
+        . "WHERE id = '" . $i . "'";
+    $sth = $pdo->prepare($sql);
+    $sth->execute();
+    return $sth->fetchAll(PDO::FETCH_OBJ);
+    $sth->closeCursor();
+    $sth = null;
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -51,16 +59,16 @@ function getTopics(PDO $pdo, $a){
             <h1 class="text-center mt-5 pt-5 mb-3">BCBB</h1>
             <ul class="nav nav-justified">
                 <li class="nav-item bg-secondary">
-                    <a class="nav-link text-white <?php if($board == "/index.php?board=General" OR $board == "/" OR $board == "/index.php"){ echo 'bg-dark';} else { echo 'bg-secondary';}; ?>" href="index.php?board=General">General</a>
+                    <a title="<?php getDescription($pdo, 1) ?>" class="nav-link text-white <?php if($board == "/index.php?board=General" OR $board == "/" OR $board == "/index.php"){ echo 'bg-dark';} else { echo 'bg-secondary';}; ?>" href="index.php?board=General">General</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white <?php if($board == "/index.php?board=Development"){ echo 'bg-dark';} else { echo 'bg-secondary';}; ?>" href="index.php?board=Development">Development</a>
+                    <a title="<?php getDescription($pdo, 2) ?>"class="nav-link text-white <?php if($board == "/index.php?board=Development"){ echo 'bg-dark';} else { echo 'bg-secondary';}; ?>" href="index.php?board=Development">Development</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white <?php if($board == "/index.php?board=Smalltalk"){ echo 'bg-dark';} else { echo 'bg-secondary';};?>" href="index.php?board=Smalltalk">Smalltalk</a>
+                    <a title="<?php getDescription($pdo, 3) ?>"class="nav-link text-white <?php if($board == "/index.php?board=Smalltalk"){ echo 'bg-dark';} else { echo 'bg-secondary';};?>" href="index.php?board=Smalltalk">Smalltalk</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white <?php if($board == "/index.php?board=Events"){ echo 'bg-dark';} else { echo 'bg-secondary';}; ?>" href="index.php?board=Events">Events</a>
+                    <a title="<?php getDescription($pdo, 4) ?>"class="nav-link text-white <?php if($board == "/index.php?board=Events"){ echo 'bg-dark';} else { echo 'bg-secondary';}; ?>" href="index.php?board=Events">Events</a>
                 </li>
             </ul>
             <?php include 'creaTopic.php';?>
@@ -98,6 +106,6 @@ function getTopics(PDO $pdo, $a){
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-        <script src="js/scrollBar.js">
+        <script src="js/scrollBar.js"></script>
     </body>
 </html>
